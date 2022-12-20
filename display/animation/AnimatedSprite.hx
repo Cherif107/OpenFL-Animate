464733f5057ee80988709f1c7e8cb77bdeb4e461
Cherry <78418245+Cherif107@users.noreply.github.com>
2022-12-20 21:14:36 +0100

    Update AnimatedSprite.hx

--- display/animation/AnimatedSprite.hx
+++ display/animation/AnimatedSprite.hx
@@ -9,17 +9,18 @@ import openfl.display.Sprite;
 import openfl.events.Event;
 
 class AnimatedSprite extends Sprite {
-    public var animation:AnimationController;
-    public var frames(get, set):Array<Frame>;
-    public var bitmap:Bitmap = new Bitmap();
+	public var animation:AnimationController;
+	public var frames(get, set):Array<Frame>;
+	public var bitmap:Bitmap = new Bitmap();
 
-    private var curBitmap:BitmapData;
-    private var focusLost:Bool = false;
-    public function new(?frames:Array<Frame>){
-        super();
+	private var curBitmap:BitmapData;
+	private var focusLost:Bool = false;
+
+	public function new(?frames:Array<Frame>) {
+		super();
 		animation = new AnimationController(this);
-        addChild(bitmap);
-        addEventListener(Event.ENTER_FRAME, updateBitmap);
+		addChild(bitmap);
+		addEventListener(Event.ENTER_FRAME, updateBitmap);
 		addEventListener(Event.DEACTIVATE, function(_) {
 			focusLost = true;
 		});
@@ -27,44 +28,49 @@ class AnimatedSprite extends Sprite {
 			focusLost = false;
 		});
 
-        if (frames != null) this.frames = frames;
-    }
+		if (frames != null)
+			this.frames = frames;
+	}
 
-    public function updateBitmap(_){
-        if (!focusLost){
-            if (animation != null && animation.curAnim != null){
-                if (curBitmap != frames[animation.curAnim.curIndex].bitmap){
-			    	curBitmap = frames[animation.curAnim.curIndex].bitmap;
-                    bitmap.bitmapData = curBitmap;
-					trace(animation.name);
-                }
-            }
-        }
-    }
-    public function set_frames(Frames:Array<Frame>):Array<Frame>{
-        animation.frames = Frames;
-        return Frames;
-    }
+	public function updateBitmap(_) {
+		if (!focusLost) {
+			if (animation != null && animation.curAnim != null) {
+				if (curBitmap != frames[animation.curAnim.curIndex].bitmap) {
+					curBitmap = frames[animation.curAnim.curIndex].bitmap;
+					bitmap.bitmapData = curBitmap;
+				}
+			}
+		}
+	}
+
+	public function set_frames(Frames:Array<Frame>):Array<Frame> {
+		animation.frames = Frames;
+		return Frames;
+	}
+
 	public function get_frames():Array<Frame> {
 		return animation.frames;
 	}
 
-	public static function fromFramesCollection(framesCollection:FlxFramesCollection):AnimatedSprite
-	{
+	public static function fromFramesCollection(framesCollection:FlxFramesCollection):AnimatedSprite {
 		var frames:Array<Frame> = [];
-        for (frame in framesCollection.frames)
-            frames.push(new Frame(frame.name, frame.paint()));
-        return new AnimatedSprite(frames);
+		for (frame in framesCollection.frames)
+			frames.push(new Frame(frame.name, frame.paint()));
+		return new AnimatedSprite(frames);
 	}
-    public static function fromSparrow(image:String, path:String):AnimatedSprite{ // forgive me for these
+
+	public static function fromSparrow(image:String, path:String):AnimatedSprite { // forgive me for these
 		return AnimatedSprite.fromFramesCollection(image, path);
-    }
+	}
+
 	public static function fromSpriteSheetPacker(image:String, path:String):AnimatedSprite {
 		return AnimatedSprite.fromFramesCollection(image, path);
 	}
+
 	public static function fromTexturePackerXml(image:String, path:String):AnimatedSprite {
 		return AnimatedSprite.fromFramesCollection(image, path);
 	}
+
 	public static function fromTexturePackerJson(image:String, path:String):AnimatedSprite {
 		return AnimatedSprite.fromFramesCollection(image, path);
 	}
